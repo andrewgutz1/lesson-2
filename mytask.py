c89f1f06f7a30d7569f3b8b6d71204611d0e0f5a
Bobby Andrew Gutang <gutangandrew16@gmail.com>
2024-09-28 10:53:39 +0800

    my task

---
+++ mytask.py
@@ -0,0 +1,13 @@
+age = [-12,13,-14,15,-16]
+username= ["juan", "pedro","james", "sagaad", "fuego"]
+for dataage in age:
+    if dataage > 0:
+        print(str(dataage) + "negative")
+    elif dataage < 0:
+        print(str(dataAge) + "positive")
+        
+for dataUser in username:
+    if dataUser == "james":
+        print("james cute")
+    else:
+        print("james none")
